26b512d78f0693bec65fa8d3c17e403e453f1634
Ratul00111 <125200238+Ratul00111@users.noreply.github.com>
2023-04-08 23:59:44 +0600

    Add files via upload

---
+++ rx00.py
@@ -0,0 +1,5 @@
+import os
+print("\033[1;92m [•] RAFI (credit-rx_world) (ROBOT) SYSTEM INSTALL. . . . \033[1;30m")
+os.system("pkg install espeak")
+os.system('espeak -a 100 "কিরে রাফি সাওয়াই"')
+print("Rafi")
